165531ce369199b160c969623f8d78c24da76b3c
Bagchi <abagchi@l160119.stjude.sjcrh.local>
2019-12-11 20:22:43 -0600

    commit

--- GGPLOT2_Code_For_Swimmers_Plot.R
+++ GGPLOT2_Code_For_Swimmers_Plot.R
@@ -7,7 +7,7 @@ dplyr::distinct() %>%
 ggplot(aes(Subject_ID, EndTime)) + # Base axis
 geom_bar(stat="identity", aes(fill=factor(Group))) + # Bar plot. Colour by Group
 geom_point(data=SHH.shapes, # Use SHH.shapes to add reponse points
-aes(Subject_ID, time,shape=ResponseType), size= 2) +
+aes(Subject_ID, time,shape=ResponseType), size= 5) +
 coord_flip() + labs(fill="Group", colour="Symbol Key", shape="Symbol Key",  # Add labels
 x="Subject ID ", y="Months since diagnosis",title="Swimmer Plot") +
 theme(plot.title = element_text(hjust = 0.5), # Put title in the middle of plot 
